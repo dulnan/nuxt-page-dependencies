663b9019ff9e0df00563f55696775034da289521
Jan Hug <me@dulnan.net>
2024-09-03 08:45:17 +0200

    chore: update eslint

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
-  rules: {
-    'vue/no-v-html': 'off',
-    'vue/no-v-text-v-html-on-component': 'off',
-    'vue/multi-word-component-names': 'off',
-  },
-}
